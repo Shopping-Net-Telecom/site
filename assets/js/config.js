c1820f021155f6482d6181c931a1394f4ed47c4a
Ricardo Negreiros <75104283+httpmerak@users.noreply.github.com>
2025-06-07 13:16:06 -0300

    ajusta telefone

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 window.siteConfig = {
   empresa: 'Shoping Net Telecom',
-  whatsapp: '5544999999999',
+  whatsapp: '5591988503676',
+  instagram: 'shopping_net123',
   endereco: 'Av Magalhães Barata, S/n, Bairro Perpétuo Socorro, Em frente ao supermercado Destak',
   cnpj: '39.763.047/0001-42',
   logo: 'assets/img/logo.png'
