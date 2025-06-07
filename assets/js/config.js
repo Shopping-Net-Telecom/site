6499eb1cc1b70cbe51332d0c98b5638284e419da
Ricardo Negreiros <75104283+httpmerak@users.noreply.github.com>
2025-06-07 13:05:21 -0300

    add files

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  empresa: 'Shoping Net Telecom',
+  whatsapp: '5544999999999',
+  endereco: 'Av Magalhães Barata, S/n, Bairro Perpétuo Socorro, Em frente ao supermercado Destak',
+  cnpj: '39.763.047/0001-42',
+  logo: 'assets/img/logo.png'
+}; 
